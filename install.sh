08538e8ef4cf64377f6f648949978fa93764533e
Helge Hoff <helge@orcalabs.no>
2018-12-12 19:37:14 +0100

    fix: update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+if [ -f ~/.vimrc ]; then
+    mv ~/.vimrc ~/.vimrc.bak
+fi
+
+if [ -f ~/.zshrc ]; then
+    mv ~/.zshrc ~/.zshrc.bak
+fi
 cp dot.vimrc ~/.vimrc
+cp dot.zshrc ~/.zshrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
